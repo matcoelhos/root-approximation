37fa53b2f6b5773e0837c78d6ddf5afdd13ccd83
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2020-06-11 09:15:07 -0300

    Addition of newton's method

--- root.py
+++ root.py
@@ -1,15 +1,15 @@
 import time
 
 def power(value,exponent):
-	if(exponent > 0):
-		return value*power(value,exponent-1)
-	elif (exponent < 0):
-		return(1/power(value,-1*(exponent)))
-	else:
-		return 1
+	ans = value
+	while(exponent > 1):
+		ans *= value
+		exponent -= 1
+	return ans
 
-def root(value, radical, tol):
-	upper = value
+#Bisection method
+def root(value, radical, tol = 1e-10):
+	upper = max(value,1)
 	lower = 0
 	actual = 0
 	while(abs(upper-lower) > tol):
@@ -27,18 +27,38 @@ def root(value, radical, tol):
 
 	return actual,abs(upper-lower)
 
+#Newton's method
+def rootn(value, radical, tol = 1e-10):
+	upper = max(value,1)
+	lower = 0
+	last = 0
+	actual = ((upper+lower)/radical)
+	while(abs(actual-last) > tol):
+		try:
+			last = actual
+			#actual = actual - (power(actual,radical) - value)/(radical*power(actual,radical-1))
+			actual = (1 - 1/radical)*actual + value/(radical*power(actual,radical-1))
+		except KeyboardInterrupt:
+			break
+	return actual,abs(actual-last)
+
 t0 = time.perf_counter()
-v1,toll = root(2,2, tol=5e-10)
+v1,toll = root(5,2)
 i1 = time.perf_counter()-t0
 
 t0 = time.perf_counter()
-v2 = 2**(1/2)
+v2,toll2 = rootn(5,2)
 i2 = time.perf_counter()-t0
 
-print('Clc value:',v1,'Time:',i1)
+t0 = time.perf_counter()
+v3 = 5**(1/2)
+i3 = time.perf_counter()-t0
+
+print('Bsc value:',v1,'Time:',i1)
 print('Tolerance reached:', toll)
-print('Ref value:',v2,'Time:',i2)
-print('Calculated error:',abs(v2-v1))
-v,_ = root(5,2, tol=1e-15)
+print('Nwt value:',v2,'Time:',i2)
+print('Tolerance reached:', toll2)
+print('Ref value:',v3,'Time:',i3)
+v,_ = rootn(5,2, tol=1e-10)
 print('Clc phi:',(1+v)/2)
 print('Ref phi:',(1+5**(1/2))/2)
